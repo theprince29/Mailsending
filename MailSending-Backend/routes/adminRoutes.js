09145e9976e3f526b7a70bc1aa9e82f65d617a31
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2025-07-27 14:05:44 +0530

    Update adminRoutes.js

--- MailSending-Backend/routes/adminRoutes.js
+++ MailSending-Backend/routes/adminRoutes.js
@@ -1,8 +1,12 @@
 import express from "express"
 import { AllUser, UpdateUser } from "../controllers/adminController.js"
-
+import {resetPasswordAdmin, verifyResetOTP,resetPassword, loginAdmin } from "../controllers/adminAuhtController.js"
 const router = express.Router()
 
 router.get("/allUser" , AllUser)
 router.put("/updateUser", UpdateUser)
+router.post("/forgot", resetPasswordAdmin)
+router.post('/verify', verifyResetOTP)
+router.post('/reset',resetPassword)
+router.post('/login', loginAdmin)
 export default router;
