1036fb700a4d0746054c930ee1619bbc7d64e205
PRINCE PAL <113759522+theprince29@users.noreply.github.com>
2025-07-27 14:04:46 +0530

    Create Admin.js

---
+++ MailSending-Backend/models/Admin.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: Date,
+  },
+});
+
+export default mongoose.model("Admin", adminSchema);
